dd73d71389c3fb8823c795fa72f0184985535295
vievegan <vieveganthaneswaran@gmail.com>
2025-02-22 13:44:36 +0530

    project initialized and landing page done

---
+++ app/_data/Lookup.jsx
@@ -0,0 +1,7 @@
+export default {
+  HeroHeading: "AI Logo Maker",
+  HeroSubheading: "Generate Logo in seconds",
+  HeroDescription:
+    "AI Logo Maker is a tool that generates a logo in seconds. You can use it to generate a logo for your startup, business, or organization. The tool is easy to use and free to use.",
+  InputTitlePlaceholder: "Enter your Logo Name",
+};
